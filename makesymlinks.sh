4812ec1343ddb05b2f61674e059f79a8b26f2331
Yoshua Wakeham <yoshwakeham@gmail.com>
2020-07-22 09:25:13 -0400

    don't use X files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,7 +11,7 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="bashrc zshrc vimrc gvimrc bash_profile spacemacs zshrc-local zpath zshenv"
-files="$files xbindkeysrc xinitrc Xresources" # linux specific
+# files="$files xbindkeysrc xinitrc Xresources" # arch linux specific
 
 ##########
 
